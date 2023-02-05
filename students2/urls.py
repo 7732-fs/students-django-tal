f46a4b5fe26b35b55284ca73226dec6c4bbbc7c0
Your Name <you@example.com>
2023-02-05 16:47:45 +0200

    django user auth

--- students2/urls.py
+++ students2/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.contrib import admin
 from . import views
 
 urlpatterns=[
+    path('', views.home, name='home'),
+    path('login', views.my_login, name='my_login'),
+    path('admin/', admin.site.urls),
     path('courses/add', views.add_course, name='add_course'),
     path('users', views.show_users, name='show_users')
 ]
--- students2/views.py
+++ students2/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Course, User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required 
 # Create your views here.
 
+def my_login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            redirect('home')
+        else:
+            return HttpResponse("unauthorized")
+    return render(request, 'login.html')
+
+def home(request):
+    return HttpResponse("hi login at <a href='/login'>here</a>")
+
+@login_required
 def add_course(request):
     if request.method=='POST':
         name=request.POST["course_name"]
         description=request.POST["description"]
         course=Course(name=name, description=description)
         course.save()
-        request.session["user"]="anonymous"
-    return render(request, "add_course.html", {"user":request.session.get("user", "not logged in")})
+    return render(request, "add_course.html", {"user":request.user })
 
 
 def show_users(request):
