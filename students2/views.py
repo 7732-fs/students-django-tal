dfc167c75d0fcbc2c991a02e1e106fe4cac41652
OferRotemm <101643338+OferRotemm@users.noreply.github.com>
2023-02-09 09:37:47 +0200

    try-except for add course

--- students2/views.py
+++ students2/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from .models import Course, User
+from django.db.utils import IntegrityError
 # Create your views here.
 
 def add_course(request):
-    if request.method=='POST':
-        name=request.POST["course_name"]
-        description=request.POST["description"]
-        course=Course(name=name, description=description)
-        course.save()
-        request.session["user"]="anonymous"
-    return render(request, "add_course.html", {"user":request.session.get("user", "not logged in")})
-
+    message = ""
+    try:
+        if request.method=='POST':
+            name=request.POST["course_name"]
+            description=request.POST["description"]
+            course=Course(name=name, description=description)
+            course.save()
+            request.session["user"]="anonymous"
+            message = f"{name} Added successfully"
+        return render(request, "add_course.html",  {"user":request.session.get("user", "not logged in"), "message":message})
+    except IntegrityError:
+        message = (f"{name} Already exists")
+        return render(request, "add_course.html", {"user":request.session.get("user", "not logged in"), "message":message})
 
 def show_users(request):
     users=User.objects.all()
