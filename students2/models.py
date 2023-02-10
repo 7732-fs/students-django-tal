74e2be77e5c1ae1d4ca83b0c18b11c0a82c38e74
OferRotemm <101643338+OferRotemm@users.noreply.github.com>
2023-02-10 15:34:50 +0200

    .

--- students2/models.py
+++ students2/models.py
@@ -2,23 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+
 class Student(models.Model):
     name = models.CharField(max_length=256)
     email = models.CharField(max_length=256)
     grade = models.IntegerField(default=0)
 
+
 class Course(models.Model):
-    name=models.CharField(max_length=256, unique=True)
-    description=models.CharField(max_length=512)
+    name = models.CharField(max_length=256, unique=True)
+    description = models.CharField(max_length=512)
     students = models.ManyToManyField(Student)
 
     def __str__(self) -> str:
         return self.name
 
+
 class User(models.Model):
-    username=models.CharField(max_length=256)
-    password=models.CharField(max_length=256)
-    role=models.CharField(max_length=256)
+    username = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    role = models.CharField(max_length=256)
 
     def __str__(self) -> str:
         return self.username
