9e4d1f07c42653b3df3b3649953d9f3ee7659791
OferRotemm <101643338+OferRotemm@users.noreply.github.com>
2023-02-10 15:33:25 +0200

    navbar and layout

--- students2/urls.py
+++ students2/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns=[
     path('courses/add', views.add_course, name='add_course'),
-    path('users', views.show_users, name='show_users')
+    path('users', views.show_users, name='show_users'),
+    path('', views.home, name='home'),
+    path('add_student',views.add_student,name='add_student')
 ]
 
 # this is a comment
+# this is Israel's comment
--- students2/views.py
+++ students2/views.py
@@ -7,50 +7,45 @@ from django.contrib import messages
 
 # Create your views here.
 
-def home(request,methods=["POST ,GET"]):
-    return render(request,"home.html")
+
+def home(request, methods=["POST ,GET"]):
+    return render(request, "home.html")
+
 
 def add_course(request):
     message = ""
     try:
-        if request.method=='POST':
-            name=request.POST["course_name"]
-            description=request.POST["description"]
-            course=Course(name=name, description=description)
+        if request.method == 'POST':
+            name = request.POST["course_name"]
+            description = request.POST["description"]
+            course = Course(name=name, description=description)
             course.save()
-            request.session["user"]="anonymous"
+            request.session["user"] = "anonymous"
             message = f"{name} Added successfully"
-        return render(request, "add_course.html",  {"user":request.session.get("user", "not logged in"), "message":message})
+            if "user" in request.session:
+                return render(request, "add_course.html", {"user": f'You are logged in as {request.session.get("user")}', "message": message})
+            else:
+                return render(request, "add_course.html", {"user": "You are not logged in",  "message": message})
+        else:
+            return render(request, "add_course.html", {"user": "You are not logged in",  "message": message})
+
     except IntegrityError:
         message = (f"{name} Already exists")
-        return render(request, "add_course.html", {"user":request.session.get("user", "not logged in"), "message":message})
-    if request.method=='POST':
-        name=request.POST["course_name"]
-        description=request.POST["description"]
-        course=Course(name=name, description=description)
-        course.save()
-        request.session["user"]="anonymous"
-        if "user" in request.session:
-            return render(request, "add_course.html", {"user":f'You are logged in as {request.session.get("user")}'})
-        else:
-            return render(request, "add_course.html", {"user":"You are not logged in"})
-    else:
-        return render(request, "add_course.html", {"user":"You are not logged in"})
+        return render(request, "add_course.html", {"user": request.session.get("user", "not logged in"), "message": message})
+
 
 def add_student(request):
-    if request.method=='POST':
-        name=request.POST["student_name"]
-        email=request.POST["student_email"]
-        grade=request.POST["student_grade"]
-        student=Student(name=name, email=email, grade=grade)
+    msg = ""
+    if request.method == 'POST':
+        name = request.POST["student_name"]
+        email = request.POST["student_email"]
+        grade = request.POST["student_grade"]
+        student = Student(name=name, email=email, grade=grade)
         student.save()
-        msg = messages.success(request,f"{student.name} Added to DB")
-    return render(request,"add_student.html", {"message":msg})
+        msg = messages.success(request, f"{student.name} Added to DB")
+    return render(request, "add_student.html", {"message": msg})
+
 
 def show_users(request):
-    users=User.objects.all()
-    return render(request, "users.html", {"users":users})
-
-
-
-
+    users = User.objects.all()
+    return render(request, "users.html", {"users": users})
