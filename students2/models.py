4b9f7c2f699632ac2fda148e34008caa8e3cd438
Your Name <you@example.com>
2023-02-19 19:48:15 +0200

    message

--- students2/models.py
+++ students2/models.py
@@ -7,6 +7,7 @@ class Student(models.Model):
     email = models.CharField(max_length=256)
     grade = models.IntegerField(default=0)
 
+
 class Course(models.Model):
     name=models.CharField(max_length=256, unique=True)
     description=models.CharField(max_length=512)
@@ -15,6 +16,12 @@ class Course(models.Model):
     def __str__(self) -> str:
         return self.name
 
+class Teacher(models.Model):
+    name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    courses=models.ManyToManyField(Course)
+
+
 class User(models.Model):
     username=models.CharField(max_length=256)
     password=models.CharField(max_length=256)
@@ -22,3 +29,8 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return self.username
+
+class Message(models.Model):
+    content=models.TextField(max_length=1024)
+    source=models.ForeignKey(Student, verbose_name=("student_message"), on_delete=models.CASCADE)
+    dest=models.ForeignKey(Teacher, verbose_name=("teacher_message"), on_delete=models.CASCADE)
